ff343cead14ccfb391f34c03860e59901589ad55
Oussema Chaari <chaari.ouss@gmail.com>
2021-10-28 14:04:59 +0100

    Improve the structure

---
+++ src/main/java/com/expertsdutest/demo/model/Credentials.java
@@ -0,0 +1,30 @@
+package com.expertsdutest.demo.model;
+
+public class Credentials {
+
+	String email;
+	String password;
+
+	public Credentials() {
+		email = "";
+		password = "";
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+}
